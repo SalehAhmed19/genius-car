a43b7cfa4b5d3723c21b3f40c95b1688617b81a1
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-04-16 05:13:53 +0600

    services added

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
 import "./Services.css";
 
 const Services = () => {
+  const [services, setService] = useState([]);
+  useEffect(() => {
+    fetch("service.json")
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
   return (
     <div>
-      <h2>Services</h2>
+      <h1 className="service-title">Our Services {services.length}</h1>
+      <div className="service-container">
+        {services.map((service) => (
+          <Service key={service.id} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
